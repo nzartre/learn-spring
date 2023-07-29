862a0cb2e30ad54931f879aeca8655163d6e1291
Nathan Yiangsuppanontr <git@zartre.com>
2023-07-29 11:28:57 +0700

    refactor: use "todos" as base for TodoController

--- src/main/java/com/zartre/hexatodo/SpringApp/handler/TodoHandler.java
+++ src/main/java/com/zartre/hexatodo/SpringApp/handler/TodoHandler.java
@@ -3,6 +3,6 @@ package com.zartre.hexatodo.SpringApp.handler;
 import com.zartre.hexatodo.SpringApp.model.TodoItem;
 import org.springframework.web.bind.annotation.RequestBody;
 
-public interface Handler {
+public interface TodoHandler {
     TodoItem createTodo(@RequestBody TodoItem item);
 }
--- src/main/java/com/zartre/hexatodo/SpringApp/handler_http/TodoController.java
+++ src/main/java/com/zartre/hexatodo/SpringApp/handler_http/TodoController.java
@@ -1,23 +1,25 @@
 package com.zartre.hexatodo.SpringApp.handler_http;
 
 import com.zartre.hexatodo.SpringApp.model.TodoItem;
-import com.zartre.hexatodo.SpringApp.handler.Handler;
+import com.zartre.hexatodo.SpringApp.handler.TodoHandler;
 import com.zartre.hexatodo.SpringApp.service.TodoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class TodoController implements Handler {
+@RequestMapping("/todos")
+public class TodoController implements TodoHandler {
     @Autowired
     private TodoService todoService;
 
     private Logger logger = LoggerFactory.getLogger(TodoController.class);
 
-    @PostMapping("/todos")
+    @PostMapping
     public TodoItem createTodo(@RequestBody TodoItem item) {
         logger.info(String.format("createTodo with body %s", item.toString()));
         return todoService.createTodo(item);
