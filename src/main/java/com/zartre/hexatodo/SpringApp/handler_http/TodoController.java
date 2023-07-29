4ab18902cfb40cddc14a6d5803c6a0c7baa73862
Nathan Yiangsuppanontr <git@zartre.com>
2023-07-29 10:19:19 +0700

    refactor: use slf4j as logger

--- src/main/java/com/zartre/hexatodo/SpringApp/handler_http/TodoController.java
+++ src/main/java/com/zartre/hexatodo/SpringApp/handler_http/TodoController.java
@@ -3,6 +3,8 @@ package com.zartre.hexatodo.SpringApp.handler_http;
 import com.zartre.hexatodo.SpringApp.model.TodoItem;
 import com.zartre.hexatodo.SpringApp.handler.Handler;
 import com.zartre.hexatodo.SpringApp.service.TodoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,9 +15,11 @@ public class TodoController implements Handler {
     @Autowired
     private TodoService todoService;
 
+    private Logger logger = LoggerFactory.getLogger(TodoController.class);
+
     @PostMapping("/todos")
     public TodoItem createTodo(@RequestBody TodoItem item) {
-        System.out.println("HttpHandler.createTodo");
+        logger.info(String.format("createTodo with body %s", item.toString()));
         return todoService.createTodo(item);
     }
 }
