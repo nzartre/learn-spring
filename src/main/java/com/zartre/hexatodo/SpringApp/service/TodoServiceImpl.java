32b3a9664ceebc078803f9bd06bb8b6ddc0a4f45
Nathan Yiangsuppanontr <git@zartre.com>
2023-07-29 15:58:15 +0700

    refactor: replace constructor with Autowired

--- src/main/java/com/zartre/hexatodo/SpringApp/service/TodoServiceImpl.java
+++ src/main/java/com/zartre/hexatodo/SpringApp/service/TodoServiceImpl.java
@@ -2,15 +2,13 @@ package com.zartre.hexatodo.SpringApp.service;
 
 import com.zartre.hexatodo.SpringApp.db.TodoDB;
 import com.zartre.hexatodo.SpringApp.model.TodoItem;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TodoServiceImpl implements TodoService {
-    private final TodoDB todoDB;
-
-    public TodoServiceImpl(TodoDB todoDB) {
-        this.todoDB = todoDB;
-    }
+    @Autowired
+    private TodoDB todoDB;
 
     @Override
     public TodoItem createTodo(TodoItem item) {
